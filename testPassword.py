00e7e8714b18a45d0307e39cf6114ba37c4f2a71
harigajmer21 <hgajmers@stud.hs-offenburg.de>
2021-01-01 21:01:30 +0100

    Remove unwanted functions in test class

--- testPassword.py
+++ testPassword.py
@@ -2,11 +2,7 @@ import unittest
 import Password
 
 class TestPasswordModule(unittest.TestCase):
-    def test1(self):
-        True
-
-    def testPassword(self):
-        True
+   
 
 if __name__ == '__main__':
     unittest.main()
